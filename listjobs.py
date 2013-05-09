f45c5b4ba21c765233bc56da2202a85e7d68d944
Jonas Gulle <jonas.gulle@gmail.com>
2013-05-09 21:54:06 +0200

    Work in progress

--- listjobs.py
+++ listjobs.py
@@ -3,8 +3,23 @@
 from config import *
 from threading import Thread
 from urllib2 import urlopen
+from HTMLParser import HTMLParser
 import xml.etree.ElementTree as ET
 import datetime, time, os, sys
+
+class MLStripper(HTMLParser):
+	def __init__(self):
+		self.reset()
+		self.fed = []
+	def handle_data(self, d):
+		self.fed.append(d)
+	def get_data(self):
+		return "".join(self.fed)
+
+def strip_description(s):
+	stripper = MLStripper()
+	stripper.feed(s)
+	return " ".join(stripper.get_data().split())
 
 def update_cache_thread():
 	tempfile = "%s.part" % CACHEFILE
@@ -43,14 +58,50 @@ def get_jobs():
 			d[k] = job.find(k).text.encode("utf-8")
 		# Convert the date string to a real date
 		d["date"] = datetime.datetime.strptime(d["date"], "%m/%d/%Y").date()
+		# Strip all HTML from the description
+		d["description"] = strip_description(d["description"])
 		result.append(d)
 	# Return the list sorted by date (descending)
 	return sorted(result, key=lambda k: k["date"], reverse=True)
 
+def format_job(job):
+	return "{date} [{apply-url}] ({title} in {location}) {description}... see {detail-url} for more\n".format(**job)
+
+def save_all(filename):
+	with open(filename, "wt") as f:
+		for job in get_jobs():
+			f.write(format_job(job))
+
+def save_latest(filename):
+	with open(filename, "wt") as f:
+		for job in get_jobs()[:20]:
+			f.write(format_job(job))
+
+def save_by_term(filename, term):
+	with open(filename, "wt") as f:
+		f.write("Searching for %s...\n" % term)
+		for job in get_jobs():
+			fields = "location region category description".split()	
+			for field in fields:
+				if term.lower() in job[field].lower():
+					f.write(format_job(job))
+
 def main():
-	jobs = get_jobs()
-	for job in jobs:
-		print job["date"],
+	if len(sys.argv) != 3:
+		print >> sys.stderr, "Usage: listjobs.py file [all|latest|search=term]"
+		return 1
+
+	filename, arg = sys.argv[1], sys.argv[2]
+
+	if arg == "all":
+		save_all(filename)
+	elif arg == "latest":
+		save_latest(filename)
+	elif "search=" in arg:
+		save_by_term(filename, arg.split("=")[1])
+	else:
+		print >> sys.stderr, "Unknown argument"
 
 if __name__ == "__main__":
-	main()
+	exit(main())
+
